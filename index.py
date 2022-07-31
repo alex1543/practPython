3721d9e2fa31d6b8a7bd3144da29aaedd4b15ffc
Alexey Subbotin <alesu1543@gmail.com>
2022-07-31 20:17:13 +0300

    func del

--- index.py
+++ index.py
@@ -14,21 +14,26 @@ cur = myconn.cursor()
 cur.execute("SET NAMES utf8;")
 cur.execute("USE test;")
 
-# обновление строки в БД
+# обновление, удаление строки в БД
 import os
 query_string = os.environ['QUERY_STRING']
-if query_string is not '':
+if query_string is not None:
     import urllib.parse
     urlGets = urllib.parse.parse_qs(query_string)
-    if urlGets['textId']!='' and urlGets['textEd1']!='' and urlGets['textEd2']!='' and urlGets['textEd3']!='':
+    if 'delid' in urlGets:
+        delid=str(urlGets['delid']).replace('[\'','').replace('\']','')
+        cur.execute("DELETE FROM files WHERE id_my = "+delid)
+        cur.execute("DELETE FROM myarttable WHERE id = "+delid)
+        myconn.commit()
+    if 'textId' in urlGets and 'textEd1' in urlGets and 'textEd2' in urlGets and 'textEd3' in urlGets:
         textId=str(urlGets['textId']).replace('[\'','').replace('\']','')
         textEd1=str(urlGets['textEd1']).replace('[\'','').replace('\']','')
         textEd2=str(urlGets['textEd2']).replace('[\'','').replace('\']','')
         textEd3=str(urlGets['textEd3']).replace('[\'','').replace('\']','')
        # print(textId, textEd1, textEd2, textEd3)
         cur.execute("UPDATE myarttable SET text='"+textEd1+"', description='"+textEd2+"', keywords='"+textEd3+"' WHERE id = "+textId)    
-        myconn.commit()
-# конец обновления в БД
+        myconn.commit()  
+# конец обновления, удаления строки из БД
 
 try: 
     cur.execute("SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC;")
@@ -38,9 +43,11 @@ try:
         iR=0
         for cell in line:
             sCellNew = str(str(cell).strip().encode('utf-8'), 'cp1251')
-            sNewView = '<td><a href="#" class="js-open-modal" data-modal="1" id=\"id'+str(iR)+'_' + str(line[0]).strip() + '">' + sCellNew + '</a></td>'
+            sNewView = '<td title="Edit"><a href="#" class="js-open-modal" data-modal="1" id=\"id'+str(iR)+'_' + str(line[0]).strip() + '">' + sCellNew + '</a></td>'
             print(sNewView);
             iR+=1
+        sNewView = '<td class="cellDel" title="Delete"><a href="index.py?delid=' + str(line[0]).strip() + '"><img src="image/delete2.png"></a></td>'
+        print(sNewView);
         print('</tr>');
 except: 
     myconn.rollback() 
